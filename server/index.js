5a649f0355fb5cc2a3c18ce6278f6776485ed37f
Esther Annorzie <86447069+estherannorzie@users.noreply.github.com>
2023-02-20 12:29:55 -0500

    Add mongoose DB connection

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql'); 
 const schema = require('./schema/schema');
-const port = process.env.PORT || '5000';
+const port = process.env.PORT;
 
+const connectDB = require('./config/db');
 const app = express();
+connectDB();
+
 app.use(
   '/graphql', 
   graphqlHTTP({
