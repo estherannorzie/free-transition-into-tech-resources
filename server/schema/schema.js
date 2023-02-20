10ea8132776bb21cb9e51266b1ae4493102c4b25
Esther Annorzie <86447069+estherannorzie@users.noreply.github.com>
2023-02-20 15:14:54 -0500

    Add a mutation to create a program

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -45,6 +45,39 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addProgram: {
+      type: ProgramType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        URL: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        type: { type: new GraphQLNonNull(GraphQLInt) },
+        country: { type: new GraphQLNonNull(GraphQLInt) },
+        lengthInWeeks: { type: GraphQLInt },
+        careerGuidance: { type: new GraphQLNonNull(GraphQLBoolean) },
+        additionalResources: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        const program = new Program({
+          name: args.name,
+          URL: args.URL,
+          description: args.description,
+          type: args.type,
+          country: args.country,
+          lengthInWeeks: args.lengthInWeeks,
+          careerGuidance: args.careerGuidance,
+          additionalResources: args.additionalResources,
+        })
+        return program.save();
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
