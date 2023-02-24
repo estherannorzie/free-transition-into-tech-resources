20463c1146f1729e94e7b2014290ebba332bacb3
Esther Annorzie <86447069+estherannorzie@users.noreply.github.com>
2023-02-24 11:44:41 -0500

    Separate program model and mutation

---
+++ server/schema/ProgramType.js
@@ -0,0 +1,25 @@
+const {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+} = require("graphql");
+
+const ProgramType = new GraphQLObjectType({
+  name: "Program",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    URL: { type: new GraphQLNonNull(GraphQLString) },
+    description: { type: new GraphQLNonNull(GraphQLString) },
+    type: { type: new GraphQLNonNull(GraphQLInt) },
+    country: { type: new GraphQLNonNull(GraphQLInt) },
+    lengthInWeeks: { type: GraphQLInt },
+    offersCareerGuidance: { type: new GraphQLNonNull(GraphQLBoolean) },
+    additionalResources: { type: GraphQLString },
+  }),
+});
+
+module.exports = ProgramType;
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,30 +1,13 @@
 const {
   GraphQLObjectType,
-  GraphQLNonNull,
   GraphQLID,
-  GraphQLInt,
-  GraphQLString,
-  GraphQLBoolean,
   GraphQLList,
   GraphQLSchema,
 } = require("graphql");
 
 const Program = require("../models/Program");
-
-const ProgramType = new GraphQLObjectType({
-  name: "Program",
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: new GraphQLNonNull(GraphQLString) },
-    URL: { type: new GraphQLNonNull(GraphQLString) },
-    description: { type: new GraphQLNonNull(GraphQLString) },
-    type: { type: new GraphQLNonNull(GraphQLInt) },
-    country: { type: new GraphQLNonNull(GraphQLInt) },
-    lengthInWeeks: { type: GraphQLInt },
-    offersCareerGuidance: { type: new GraphQLNonNull(GraphQLBoolean) },
-    additionalResources: { type: GraphQLString },
-  }),
-});
+const ProgramType = require("../schema/ProgramType");
+const ProgramMutation = require("../mutations/ProgramMutation");
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -45,84 +28,7 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
-const mutation = new GraphQLObjectType({
-  name: "Mutation",
-  fields: {
-    addProgram: {
-      type: ProgramType,
-      args: {
-        name: { type: new GraphQLNonNull(GraphQLString) },
-        URL: { type: new GraphQLNonNull(GraphQLString) },
-        description: { type: new GraphQLNonNull(GraphQLString) },
-        type: { type: new GraphQLNonNull(GraphQLInt) },
-        country: { type: new GraphQLNonNull(GraphQLInt) },
-        lengthInWeeks: { type: GraphQLInt },
-        offersCareerGuidance: { type: new GraphQLNonNull(GraphQLBoolean) },
-        onHiatus: { type: new GraphQLNonNull(GraphQLBoolean) },
-        additionalResources: { type: GraphQLString },
-      },
-      resolve(parent, args) {
-        const program = new Program({
-          name: args.name,
-          URL: args.URL,
-          description: args.description,
-          type: args.type,
-          country: args.country,
-          lengthInWeeks: args.lengthInWeeks,
-          offersCareerGuidance: args.offersCareerGuidance,
-          onHiatus: args.onHiatus,
-          additionalResources: args.additionalResources,
-        });
-        return program.save();
-      },
-    },
-    deleteProgram: {
-      type: ProgramType,
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLID) },
-      },
-      resolve(parent, args) {
-        return Program.findByIdAndRemove(args.id);
-      },
-    },
-    updateProgram: {
-      type: ProgramType,
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLID) },
-        name: { type: GraphQLString },
-        URL: { type: GraphQLString },
-        description: { type: GraphQLString },
-        type: { type: GraphQLInt },
-        country: { type: GraphQLInt },
-        lengthInWeeks: { type: GraphQLInt },
-        offersCareerGuidance: { type: GraphQLBoolean },
-        onHiatus: { type: GraphQLBoolean },
-        additionalResources: { type: GraphQLString },
-      },
-      resolve(parent, args) {
-        return Program.findByIdAndUpdate(
-          args.id,
-          {
-            $set: {
-              name: args.name,
-              URL: args.URL,
-              description: args.description,
-              type: args.type,
-              country: args.country,
-              lengthInWeeks: args.lengthInWeeks,
-              offersCareerGuidance: args.offersCareerGuidance,
-              onHiatus: args.onHiatus,
-              additionalResources: args.additionalResources,
-            }
-          },
-          { new: true },
-        );
-      },
-    }
-  },
-});
-
 module.exports = new GraphQLSchema({
   query: RootQuery,
-  mutation,
+  mutation: ProgramMutation,
 });
