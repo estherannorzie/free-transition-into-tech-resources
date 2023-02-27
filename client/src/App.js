5927043822ca557feeb03391e77910573d063ff5
Esther Annorzie <86447069+estherannorzie@users.noreply.github.com>
2023-02-27 11:54:50 -0500

    Add react router to app and create path for homepage and programs

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Programs from "./components/pages/Programs";
 
 function App() {
   return (
     <>
-      <Home />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/programs" element={<Programs />} />
+      </Routes>
     </>
   );
 }
