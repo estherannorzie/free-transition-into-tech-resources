e5a474d37be783593289ea2fb05e7815708d86fd
Esther Annorzie <86447069+estherannorzie@users.noreply.github.com>
2023-02-27 10:43:00 -0500

    Create homepage and header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Are you transitioning into tech on a budget?</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../Header";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <p>
+        This site offers some free tech resources that can help you kickstart
+        your technical career!
+      </p>
+    </>
+  );
+};
+
+export default Home;
