2b3de8ca0d63243dcd062fec3e26ab7ddc050a68
Esther Annorzie <86447069+estherannorzie@users.noreply.github.com>
2023-03-07 11:42:06 -0500

    Render all programs

--- client/src/pages/Programs.jsx
+++ client/src/pages/Programs.jsx
@@ -1,8 +1,43 @@
+import { gql, useQuery } from "@apollo/client";
+import ProgramCard from "../components/ProgramCard";
+
 const Programs = () => {
+  const GET_PROGRAMS = gql`
+    query getPrograms {
+      programs {
+        id
+        name
+        specialty
+        URL
+        description
+        type
+        country
+        lengthInWeeks
+        offersCareerGuidance
+        onHiatus
+        additionalResources
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(GET_PROGRAMS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Something Went Wrong</p>;
+
   return (
-    <header>
-      <h1>Programs</h1>
-    </header>
+    <>
+      <header>
+        <h1>Programs</h1>
+      </header>
+
+      {!loading && !error && (
+        <>
+          {data.programs.map((program) => (
+            <ProgramCard key={program.id} program={program} />
+          ))}
+        </>
+      )}
+    </>
   );
 };
 
