b68caabcc233627988fefbbcb5ea3a25e5238688
Esther Annorzie <86447069+estherannorzie@users.noreply.github.com>
2023-03-08 15:49:04 -0500

    Add modal to card to view more program information

--- client/src/components/ProgramCard.jsx
+++ client/src/components/ProgramCard.jsx
@@ -5,10 +5,24 @@ import {
   CardActions,
   Typography,
   Button,
+  Modal,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const ProgramCard = ({ program }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const countries = [
+    "United States",
+    "Canada",
+    "United Kingdom",
+    "Worldwide",
+    "Other",
+  ];
+
   return (
     <Box
       sx={{
@@ -19,10 +33,41 @@ const ProgramCard = ({ program }) => {
       <Card sx={{ minWidth: "250px" }}>
         <CardContent>
           <Typography variant="h6">{program.name}</Typography>
-          <Typography variant="body2">{program.description}</Typography>
+          <Typography variant="body2">{program.specialty}</Typography>
         </CardContent>
 
         <CardActions>
+          <Button onClick={handleOpen}>Details</Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: 400,
+                bgcolor: "background.paper",
+                border: "2px solid #000",
+                borderRadius: "5px",
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                {program.description}
+              </Typography>
+              <Typography>Country: {countries[program.country]}</Typography>
+            </Box>
+          </Modal>
+
           <Button size="small">
             <Link
               target="_blank"
